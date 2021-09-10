c2a39c386e1e5d11ca12a4d9a8e4dec20eb773e2
ShemBrooklyne <shemnyatti@gmail.com>
2021-09-10 15:44:52 +0300

    Line charts for random data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
-const routes: Routes = [];
+//routes
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+  children: [{
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
